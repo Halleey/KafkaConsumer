c8f70a483b69f17082871de9e368a8bac99b4529
hallyson <aparecidohallyson@gmail.com>
2025-02-11 15:50:37 -0300

    defining breakdowns

--- src/main/java/com/example/consumer/consumers/ListenerConsumer.java
+++ src/main/java/com/example/consumer/consumers/ListenerConsumer.java
@@ -1,15 +1,35 @@
 package com.example.consumer.consumers;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ListenerConsumer {
 
 
-    @KafkaListener(groupId =  "group-1", topics = "str-topic", containerFactory = "configurer")
-    public void listener(String message) {
-        System.out.println("Listener :::Receive message {}" + message);
+    @KafkaListener(
+            groupId = "group-1",
+            topicPartitions = @TopicPartition(
+                    topic = "str-topic",
+                    partitions = {"0"} // Escutando apenas a partição 0
+            ),
+            containerFactory = "configurer"
+    )
+    public void listenerPartition0(String message) {
+        System.out.println("Listener Partition 0 ::: " + message);
+    }
+
+    @KafkaListener(
+            groupId = "group-1",
+            topicPartitions = @TopicPartition(
+                    topic = "str-topic",
+                    partitions = {"1"} // Escutando apenas a partição 1
+            ),
+            containerFactory = "configurer"
+    )
+    public void listenerPartition1(String message) {
+        System.out.println("Listener Partition 1 ::: " + message);
     }
 
 }
